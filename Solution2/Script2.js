600c7dc572623c2b96f16247f937535d1f6ec3c1
ymanzyukova <ymanzyukova@torontomu.ca>
2024-10-09 23:29:36 -0400

    final

--- Solution2/Script2.js
+++ Solution2/Script2.js
@@ -1,20 +1,34 @@
 // JavaScript source code
 
 function changeBackground(color) {
-   document.body.style.background = color;
+    document.body.style.background = color;
 }
 
-window.addEventListener("load",function() { changeBackground('#beffa8') });
-
-   function firsteventhandler() {
-       var myInput = prompt("What is your name?") ;
-       var element = document.getElementById("myInput");
-       element.innerText=myInput;
+window.addEventListener("load", function () { changeBackground('#beffa8') });
+function firsteventhandler() {
+    var myInput = prompt("What is your name?");
+    var element = document.getElementById("myInput");
+    element.innerText = myInput;
 
 }
-   function secondeventhandler() {alert("yay");}
-   document.getElementById( "Img" ).onclick = function() {
-    // img clicked
-};
+function secondeventhandler() { alert("yay"); }
 
+function imgClick() {
+    var element = document.getElementById("myImage");
+    var oldWidth = element.width;
+    element.style.width = (oldWidth + 40)+'px';
+}
 
+function myFunction() { document.getElementById("myDropdown").classList.toggle("show"); }
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
