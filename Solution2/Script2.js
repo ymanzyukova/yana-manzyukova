3bffdb9d1defec4d775a6f657bf80a97a29c0d80
ymanzyukova <ymanzyukova@torontomu.ca>
2024-10-09 21:20:34 -0400

    trying

--- Solution2/Script2.js
+++ Solution2/Script2.js
@@ -1,2 +1,20 @@
 // JavaScript source code
-alert("Hello World!")
+
+function changeBackground(color) {
+   document.body.style.background = color;
+}
+
+window.addEventListener("load",function() { changeBackground('#beffa8') });
+
+   function firsteventhandler() {
+       var myInput = prompt("What is your name?") ;
+       var element = document.getElementById("myInput");
+       element.innerText=myInput;
+
+}
+   function secondeventhandler() {alert("yay");}
+   document.getElementById( "Img" ).onclick = function() {
+    // img clicked
+};
+
+
