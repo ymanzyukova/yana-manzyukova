e5dda7d57e86c467495fc6d217962bb0661bf41e
ymanzyukova <ymanzyukova@torontomu.ca>
2024-11-13 21:14:41 -0500

    Add files via upload

--- form-handler.js
+++ form-handler.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 //lab8
-function increaseTextSize() {
+function increaseTextsize() {
     document.body.style.fontSize = "1.2em";
 }
 function toggleColors() {
