a842a2bf1968894f5ec7dff64d828aa93ff2776a
ymanzyukova <ymanzyukova@torontomu.ca>
2024-11-13 20:54:35 -0500

    Add files via upload

---
+++ form-handler.js
@@ -0,0 +1,28 @@
+// JavaScript source code
+//form handler for storing data to a local storage
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("contactForm");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        //prevents page reload when submitted
+
+        //collect form data
+        const formData = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            gender: document.getElementById('input[name="gender"]:checked')?.value || "",
+            preferences: Array.from(document.querySelectorAll('input[name="preferences"]:checked')).map(input => input.value),
+            favColor: document.getElementById("favColor").value
+            message: document.getElementById("message").value
+        };
+        //store data on local storage
+        localStorage.setItem("contactFormData", JSON.stringify(formData));
+        alert("Data submitted successfully");
+    });
+    //clear button handler
+    document.getElementById("clear").addEventListener("click", () => {
+        form.reset(); //clears form fields
+        localStorage.removeItem("contactFormData"); //clears local storage
+    });
+});
